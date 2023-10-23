02b70f01b3948bc40345975eb6a196414a9a3706
Mayur Bandil <76209691+mayurbandil@users.noreply.github.com>
2023-10-23 08:58:10 +0530

    Time: 0 ms (100%), Space: 6.6 MB (13.31%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+
+        if(n==1){
+            return true;
+        }
+
+        float ans = log(n)/log(4);
+
+
+        return ans==floor(ans);
+    }
+};
